40f4010f48b28dcecf33a5128c0b909220806c37
Junior García <junior.sacarias.garcia.diaz@volenday.com>
2024-06-17 11:58:57 +0100

    docs: canUseDom & getDOMRangeRect.jsx

--- src/common/utils/canUseDom.jsx
+++ src/common/utils/canUseDom.jsx
@@ -1,4 +1,4 @@
-export const CAN_USE_DOM =
-	typeof window !== 'undefined' &&
-	typeof window.document !== 'undefined' &&
-	typeof window.document.createElement !== 'undefined';
+export const CAN_USE_DOM = // Check if the window object is defined.
+	typeof window !== 'undefined' && // Check if the document object is defined.
+	typeof window.document !== 'undefined' && // Check if the createElement method is defined.
+	typeof window.document.createElement !== 'undefined'; // Check if the addEventListener method is defined.
--- src/common/utils/getDOMRangeRect.jsx
+++ src/common/utils/getDOMRangeRect.jsx
@@ -1,16 +1,16 @@
-export function getDOMRangeRect(nativeSelection, rootElement) {
-	const domRange = nativeSelection.getRangeAt(0);
+export function getDOMRangeRect(nativeSelection, rootElement) { // Function that returns the bounding rectangle of a DOM range.
+	const domRange = nativeSelection.getRangeAt(0); // Get the range of the native selection.
 
-	let rect;
+	let rect; // Create a variable to store the bounding rectangle.
 
-	if (nativeSelection.anchorNode === rootElement) {
-		let inner = rootElement;
-		while (inner.firstElementChild != null) {
-			inner = inner.firstElementChild;
+	if (nativeSelection.anchorNode === rootElement) { // If the anchor node is the root element.
+		let inner = rootElement; // Set the inner element to the root element.
+		while (inner.firstElementChild != null) { // While the inner element has a first child element.
+			inner = inner.firstElementChild; // Set the inner element to the first child element.
 		}
-		rect = inner.getBoundingClientRect();
+		rect = inner.getBoundingClientRect(); // Get the bounding rectangle of the inner element.
 	} else {
-		rect = domRange.getBoundingClientRect();
+		rect = domRange.getBoundingClientRect(); // Get the bounding rectangle of the range.
 	}
 
 	return rect;
