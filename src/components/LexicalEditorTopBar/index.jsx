1e237265715a3b2eeb2ffaf802c7fb8dceeb5d92
Junior García <junior.sacarias.garcia.diaz@volenday.com>
2024-06-17 09:23:14 +0100

    Refactor LexicalEditorTopBar component

--- src/components/LexicalEditorTopBar/index.jsx
+++ src/components/LexicalEditorTopBar/index.jsx
@@ -1,47 +1,51 @@
-import { Grid } from "@mui/material";
-import toolbarIconsList from "./toolbarIconsList";
-import useOnClickListener from "./useOnClickListener";
-import { createPortal } from "react-dom";
-import FloatingLinkEditor from "./FloatingLinkEditor";
+import { createPortal } from 'react-dom';
+import { Grid, styled } from '@mui/material';
+import FloatingLinkEditor from './FloatingLinkEditor';
+import toolbarIconsList from './toolbarIconsList';
+import useOnClickListener from './useOnClickListener';
+
+/**
+ * useOnClickListener is a custom hook that handles the click event of the toolbar icons.
+ * isIconSelected is a function that checks if the icon is selected.
+ * LexicalEditorTopBar is a functional component that renders the toolbar icons and the link editor
+ * ToolbarGridContainer is a styled component that renders the toolbar grid containerWithin this component the different icons for the toolbar are rendered, in addition each one receives an onClick which activates the function associated with the icon and a color which can be changed by replacing warning
+ * modal is a prop that is used to render the modal component
+ * isLink is a prop that is used to render the link editor component
+ */
+
+const ToolbarGridContainer = styled(Grid)(({ theme }) => ({
+	justifyContent: 'space-between',
+	spacing: 2,
+	alignItems: 'center',
+	background: 'white'
+}));
+
+const IconGridContainer = styled(Grid)(({ theme }) => ({
+	cursor: 'pointer'
+}));
 
 const LexicalEditorTopBar = () => {
-  const { onClick, selectedEventTypes, blockType, isLink, editor, modal } =
-    useOnClickListener();
+	const { onClick, selectedEventTypes, blockType, isLink, editor, modal } = useOnClickListener();
 
-  const isIconSelected = (plugin) =>
-    selectedEventTypes.includes(plugin.event) ||
-    blockType.includes(plugin.event);
+	const isIconSelected = plugin => selectedEventTypes.includes(plugin.event) || blockType.includes(plugin.event);
 
-  return (
-    <Grid
-      container
-      justifyContent="space-between"
-      spacing={2}
-      alignItems="center"
-      sx={{ background: "white", py: 1.5, px: 0.5 }}
-    >
-      {toolbarIconsList.map((plugin) => (
-        <Grid
-          key={plugin.id}
-          sx={{
-            cursor: "pointer",
-          }}
-          item
-        >
-          {
-            <plugin.Icon
-              sx={plugin.iconSx}
-              onClick={() => onClick(plugin.event)}
-              color={isIconSelected(plugin) ? "secondary" : undefined}
-            />
-          }
-        </Grid>
-      ))}
-      {modal}
-      {isLink &&
-        createPortal(<FloatingLinkEditor editor={editor} />, document.body)}
-    </Grid>
-  );
+	return (
+		<ToolbarGridContainer container py={2} px={1}>
+			{toolbarIconsList.map(plugin => (
+				<IconGridContainer key={plugin.id} item>
+					{
+						<plugin.Icon
+							sx={plugin.sx}
+							onClick={() => onClick(plugin.event)}
+							color={isIconSelected(plugin) ? 'warning' : undefined}
+						/>
+					}
+				</IconGridContainer>
+			))}
+			{modal}
+			{isLink && createPortal(<FloatingLinkEditor editor={editor} />, document.body)}
+		</ToolbarGridContainer>
+	);
 };
 
 export default LexicalEditorTopBar;
